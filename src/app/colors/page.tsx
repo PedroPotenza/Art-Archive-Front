90c31c9c5471bb3b7698da2ca4f914eb426e82ec
Pedro Potenza <pedrohpotenza@gmail.com>
2024-06-09 18:56:39 -0300

    🚧 WIP: color study view

--- src/app/colors/page.tsx
+++ src/app/colors/page.tsx
@@ -3,10 +3,16 @@ import { colors } from "./colors";
 
 export default function Page() {
   return (
-    <div className="w-full flex justify-center items-center overflow-auto">
-      <div className="flex flex-col gap-0 w-full p-20">
+    <div className="w-full flex justify-center overflow-auto">
+      <div className="flex flex-row justify-between w-full p-20 gap-1">
         {colors.map((color, index) => (
-          <div key={index} className="w-full h-8 bg-gray-300" style={{ backgroundColor: color }}></div>
+          <div key={index} className="flex flex-col w-full">
+            {color.map((shade, index) => (
+              <div key={index} className="flex justify-center">
+                <div className="w-full h-2 bg-gray-200" style={{ backgroundColor: shade }}></div>
+              </div>
+            ))}
+          </div>
         ))}
       </div>
     </div>
