eb6b4d0e237c718c699664e2efc12e578224debf
Pedro Potenza <pedrohpotenza@gmail.com>
2024-08-29 15:14:33 -0300

    🎨 style(authentication flow): update login page UI

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import React, { useState } from "react";
 import { createSession } from "../../actions/authActions";
 import { signInEmailAndPassword, signInGoogle } from "../../libs/firebase/auth";
 import "../globals.css";
-import React from "react";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -31,55 +30,67 @@ export default function Login() {
 
   return (
     <div className=" flex justify-center items-center w-full h-[100vh] p-16">
-      <div className="flex flex-col w-[500px] h-fit border-4 border-almost-black rounded-2xl">
-        <div className="flex bg-almost-black w-full h-16 p-4 justify-start items-center">
-          <p className="text-almost-white text-2xl">Welcome back!</p>
+      <div className="flex flex-col w-[400px] h-fit border-4 border-almost-black rounded-2xl shadow-2xl">
+        <div className="flex bg-almost-black w-full h-16 p-4 justify-center items-center">
+          <p className="text-almost-white text-2xl font-semibold">Welcome Back!</p>
         </div>
 
-        <div className="flex flex-col w-full h-[calc(100%-4rem)] p-4 gap-4">
-          <div className="flex flex-col w-full h-16">
-            <label className="text-almost-black text-lg">Email</label>
+        <div className="flex flex-col w-full p-4 ">
+          <div className="flex flex-col w-full gap-1 mb-4">
+            <label className="text-almost-black text-xl font-semibold">Email</label>
             <input
-              className="w-full h-10 border-2 border-almost-black rounded-md bg-almost-white"
+              className="w-full h-fit border-2  border-almost-black focus-visible:border-golden-yellow-darker focus-visible:shadow-md rounded-md bg-almost-white text-lg px-2 py-1"
               type="email"
               onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>
 
-          <div className="flex flex-col w-full h-16">
-            <label className="text-almost-black text-lg">Password</label>
+          <div className="flex flex-col w-full gap-1">
+            <label className="text-almost-black text-xl font-semibold">Password</label>
             <input
-              className="w-full h-10 border-2 border-almost-black rounded-md bg-almost-white"
+              className="w-full h-fit border-2  border-almost-black focus-visible:border-golden-yellow-darker focus-visible:shadow-md rounded-md bg-almost-white text-lg px-2 py-1"
               type="password"
               onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
 
-          <div className="flex flex-row w-full h-16 justify-start items-center">
-            <a href="#" className="text-almost-black text-lg">
+          <div className="flex flex-row w-full justify-between items-start">
+            <span className="text-almost-black text-sm font-semibold mt-1 ml-2 hover:underline hover:text-golden-yellow-dark">
               Forgot password?
-            </a>
-          </div>
+            </span>
 
-          <div className="flex w-full h-16">
-            <button onClick={handleSignInEmailAndPassword} disabled={!email || !password}>
-              Login
+            <button
+              onClick={handleSignInEmailAndPassword}
+              disabled={!email || !password}
+              className="group relative overflow-hidden border-white w-fit px-4 text-lg mt-4 focus:outline-none focus:ring-0 focus-visible:outline-none focus-visible:ring-0"
+            >
+              <div
+                className={`absolute inset-0 w-0 ${
+                  !email || !password
+                    ? ""
+                    : "bg-golden-yellow-dark transition-all duration-[750ms] ease-out group-hover:w-full"
+                }`}
+              ></div>
+              <span className="relative text-white group-hover:text-white">Login</span>
             </button>
           </div>
 
-          <div className="flex w-full h-16 justify-center items-center">
-            <p className="text-almost-black text-lg">Or</p>
+          <div className="flex w-full justify-center items-center my-2">
+            <p className="text-almost-black text-lg font-semibold">Or</p>
           </div>
 
-          <div className="flex w-full h-16">
-            <button className="" onClick={handleSignInGoogle}>
-              Login with Google
-            </button>
-          </div>
+          {/* <img src="../../assets/google-icon.png" alt="google-icon" className="w-8 h-8" /> */}
+          <button
+            className="group relative overflow-hidden border-white text-lg w-full focus:outline-none focus:ring-0 focus-visible:outline-none focus-visible:ring-"
+            onClick={handleSignInGoogle}
+          >
+            <div className="absolute inset-0 w-0 bg-golden-yellow-dark transition-all duration-[750ms] ease-out group-hover:w-full"></div>
+            <span className="relative text-white group-hover:text-white">Login with Google</span>
+          </button>
 
-          <div className="flex w-full h-16 justify-center items-center">
+          <div className="flex w-full justify-center items-center mt-4">
             <p className="text-almost-black text-lg">Don&apos;t have an account?</p>
             <a href="/signUp" className="text-almost-black text-lg ml-2">
               Sign up
